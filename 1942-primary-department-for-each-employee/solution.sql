8784112f6b507fa41a0dfd7fa8b339502b432417
Yaroslav <109038517+yognevoy@users.noreply.github.com>
2026-02-08 17:06:08 +0000

    Sync LeetCode submission Runtime - 503 ms (76.08%), Memory - 0.0B (100.00%)

---
+++ 1942-primary-department-for-each-employee/solution.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT employee_id, department_id
+FROM Employee
+GROUP BY employee_id
+HAVING COUNT(*) = 1
+
+UNION ALL
+
+SELECT employee_id, department_id
+FROM Employee
+WHERE primary_flag = 'Y'
+GROUP BY employee_id
